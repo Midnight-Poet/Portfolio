5153b26815c7e75a33c1db4b7167321e71d9cebf
Midnight-Poet <kaydeymyrtle@gmail.com>
2024-09-19 12:29:52 +0100

    Hopefully it be final

--- script.js
+++ script.js
@@ -11,36 +11,38 @@ navigator.addEventListener('click', () => {
     }
 })
 
-let slide = document.querySelectorAll('.slide-content')
+let slide = document.querySelector('.slide')
 let next = document.querySelector('.nxt-btn')
 let previous = document.querySelector('.prev-btn')
 
-// next.addEventListener('click', () => {
-//     slide.scrollBy(200, 0)
-// })
-    let x = 0
-    let index = 1;
+let x = 0
+let index = 1
 next.addEventListener('click', () => {
-    index += 2;
-    if (index > slide.length) {
-        index = 1
-    }
-    for (i = 0; i < slide.length; i++) {
-        slide[i].style.display = "none";  
-    }
-    slide[index - 1].style.display = "block";
-    slide[index].style.display = "block";
+    // index += 1
+    // if (index > slide.length) {
+    //     index = 1
+    // }
+    // for (i = 0; i < slide.length; i++) {
+    //     slide[i].style.display = "none";  
+    // }
+    // slide[index - 1].style.display = "block";
+    // slide[index].style.display = "block";
+    slide.appendChild(slide.children[0])
 })
 
 previous.addEventListener('click', () => {
-    index -= 2
-    if (index < 1) {
-        index = slide.length
-    }
-    for (i = 0; i < slide.length; i++) {
-        slide[i].style.display = "none";  
-    }
-    // console.log(index);
-    slide[index - 2].style.display = "block";
-    slide[index - 1].style.display = "block";
+    // index -= 1
+    // if (index < 1) {
+    //     index = slide.length
+    // }
+    // for (i = 0; i < slide.length; i++) {
+    //     slide[i].style.display = "none";  
+    // }
+    // slide[index - 1].style.display = "block";
+    // slide[index].style.display = "block";
+    slide.prepend(slide.children[slide.children.length -1])
 })
+// console.log('otherwise');
+function scrolls() {
+    document.body.onscroll.classList.toggle(' active')
+}
